ed458b1f3a05cd0185b29f6eb8798d20c1cb1fc6
purin52002 <purin7635@gmail.com>
2020-01-19 17:19:36 +0900

    Update bit_decoder.py

--- ParameterOptimizer/bit_decoder.py
+++ ParameterOptimizer/bit_decoder.py
@@ -4,4 +4,11 @@ import typing
 
 
 class BitDecoder(metaclass=ABCMeta):
+    '''
+    implement
+    ---
+    def decode(self, bit_list: list) -> Param
+    '''
+    @abstractmethod
+    def decode(self, bit_list: list) -> Param:
         pass

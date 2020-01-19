a790018402922de1e7aec3c629db248d4402b400
purin52002 <purin7635@gmail.com>
2020-01-19 17:19:18 +0900

    Update bit_decoder.py

--- ParameterOptimizer/bit_decoder.py
+++ ParameterOptimizer/bit_decoder.py
@@ -2,7 +2,6 @@ from abc import ABCMeta, abstractclassmethod
 import typing
 
 
+
 class BitDecoder(metaclass=ABCMeta):
-    @abstractclassmethod
-    def decode(self, bit_list: list) -> typing.Any:
         pass
